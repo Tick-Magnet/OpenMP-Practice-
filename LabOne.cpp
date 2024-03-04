0626b61fffc700a23750738802d819a4881d14e2
Tick Magnet <94925671+Tick-Magnet@users.noreply.github.com>
2024-03-04 11:21:20 -0600

    Update LabOne.cpp

--- LabOne.cpp
+++ LabOne.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <omp.h>
 
+//Compilation Syntax: g++ LabOne.cpp -o lab -fopenmp
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
   
     char *p;
-    int tid, threads;
+    int tid, threads, i, r;
     
+    i = 0;
+
     //Check for Input Argument 
     if (argc != 2) {
         cout << "Incorrect Input Arguments" << endl;
@@ -19,9 +22,34 @@ int main(int argc, char* argv[]) {
 
     omp_set_num_threads(threads);  
 
-    #pragma omp parallel private(tid)
+    #pragma omp parallel private(tid) shared(i)
     {
         int tid = omp_get_thread_num();
+
+        if(tid % 2 == 0) {
+            #pragma omp atomic 
+                i--;
+        } else {
+            #pragma omp atomic 
+                i++;
+        }
+
+        
+
+        printf("i: %d\n", i);
+        //printf("Thread ID: %d, %d\n", tid, i);
+        #pragma omp flush 
+    }
+    printf("Final i: %d\n", i);
+    return 0;
+} 
+
+
+
+/* 
+
+        i = 0;
+
         #pragma omp critical 
         {
             #pragma omp master
@@ -31,9 +59,12 @@ int main(int argc, char* argv[]) {
                 cout << "Total Threads: " << total_threads << endl;
                 cout << "Master ";
             }
-            cout << "Thread ID: " << tid << endl;
+
+            #pragma omp atomic 
+                i++;
+            
+            
+            cout << "Thread ID: " << tid << " int: " << i << endl;
         }
-    }
-    
-    return 0;
-} 
+        
+*/
